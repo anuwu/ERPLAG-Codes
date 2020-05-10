60a6a0352542880bd88d0d0894472d0c65d7ebab
anwesh0304 <anwesh0304@gmail.com>
2020-05-10 23:59:38 +0530

    Testing erplag from the shell.

--- Expr_TC/expr1.asm
+++ Expr_TC/expr1.asm
@@ -11,134 +11,193 @@ main:
 		PUSH RBP
 		MOV RBP, RSP
 
-		SUB RSP, 6											; making space for declaration
-
-		MOV AX, 4
-		MOV [RBP - 6], AX										; store variable
-
-		SUB RSP, 12											; making space for declaration
-
-
-		MOV AX, 0
-		MOV BX, [RBP - 6]
-		CALL @dynamicDeclCheck										; checking dynamic array declaration limits
-
-
-		PUSH BX												; saving register for malloc
-		PUSH AX												; saving register for malloc
-		CALL malloc
-		MOV [RBP - 18], RAX
-		POP AX
-		MOV [RBP - 8], AX
-		POP BX
-		MOV [RBP - 10], BX
+		SUB RSP, 4											; making space for declaration
 
 		SUB RSP, 6											; making space for declaration
 
-		MOV AX, 1
-		MOV [RBP - 2], AX										; store variable
+		SUB RSP, 6											; making space for declaration
 
-		MOV AX, 2
-		MOV [RBP - 4], AX										; store variable
+		MOV RDI, @inputIntArrPrompt
+		MOV BX, 4
+		MOV CX, 6
+		CALL @printGetArrPrompt
 
-		MOV AX, [RBP - 4]
-		MOV [RBP - 2], AX										; store variable
+		MOV RDI, RBP
+		SUB RDI, 10
+		MOV DX, CX
+		SUB DX, BX
+		ADD DX, 1
+		ADD DX, DX
+		MOVSX RDX, DX
+		CALL @getArr
 
-		MOV RDI, [RBP - 18]
-		MOV BX, 0
-		MOV CX, [RBP - 4]
-		MOV DX, [RBP - 10]
-		CALL @boundCheck										; checking array index bound
-		MOV AX, [RDI + RBX]
+		MOV RDI, RBP
+		SUB RDI, 10
+		MOV CX, 4
+		MOV DX, 6
+		SUB DX, CX
+		ADD DX, 1
+		ADD DX, DX
+		CALL @printIntegerArr
 
-		MOV RDI, [RBP - 18]
-		MOV BX, 0
-		MOV CX, [RBP - 2]
-		MOV DX, [RBP - 10]
-		CALL @boundCheck										; checking array index bound
-		MOV [RDI + RBX], AX										; store variable
+		MOV CX, 4
+		MOV DX, 6
+		MOV RDI, RBP
+		SUB RDI, 16
+		MOV RSI, RBP
+		SUB RSI, 10
+		SUB DX, CX
+		ADD DX, 1
+		ADD DX, DX
+		CALL @asgnArr
 
+		MOV RDI, RBP
+		SUB RDI, 16
+		MOV CX, 4
+		MOV DX, 6
+		SUB DX, CX
+		ADD DX, 1
+		ADD DX, DX
+		CALL @printIntegerArr
 
-
-		MOV RDI, [RBP - 18]
-		CALL free
 
 		MOV RSP, RBP
 		POP RBP
+		MOV RAX, 1
+		MOV RBX, 0
+		INT 0x80
+
+;--------------------------------------------------------------------------------------------------
+
+;--------------------------------------------------------------------------------------------------
+
+@printGetArrPrompt:
+		MOV SI, CX
+		SUB SI, BX
+		ADD SI, 1
+		MOVSX RSI, SI
+		XOR RAX, RAX
+		PUSH BX
+		PUSH CX
+		CALL printf
+		POP CX
+		POP BX
+
+		MOV RDI, @leftRange
+		MOVSX RSI, BX
+		XOR RAX, RAX
+		PUSH BX
+		PUSH CX
+		CALL printf
+		POP CX
+		POP BX
+
+		MOV RDI, @rightRange
+		MOVSX RSI, CX
+		XOR RAX, RAX
+		PUSH BX
+		PUSH CX
+		CALL printf
+		POP CX
+		POP BX
+
 		ret
 
-;--------------------------------------------------------------------------------------------------
+@getArr:
+		MOV RAX, RSP
+		AND RAX, 15
+		ADD RAX, 8
+		SUB RSP, RAX
+		PUSH RAX
 
-;--------------------------------------------------------------------------------------------------
+		PUSH RDI
+		MOV RCX, 0
 
-@boundCheck:
-		CMP CX, BX
-		JGE .leftLim
-		CALL @boundERROR
+	.getArrLoop:								; getting array
+		MOV RDI, @inputInt
+		MOV RSI, RSP
+		SUB RSI, 24
+		PUSH RCX
+		PUSH RDX
+		PUSH RSI
+		CALL scanf
+		POP RSI
+		POP RDX
+		POP RCX
+		MOV RBX, RCX
+		MOV AX, [RSP - 24]
+		POP RDI
+		PUSH RDI
+		MOV [RDI + RBX], AX
 
-	.leftLim:
-		CMP DX, CX
-		JGE .rightLim
-		CALL @boundERROR
+		ADD RCX, 2
+		CMP RCX, RDX
+		JNE .getArrLoop
 
-	.rightLim:
-		SUB CX, BX
-		ADD CX, CX
+		POP RDI
+		POP RAX
+		ADD RSP, RAX
+
+		ret
+
+@printIntegerArr:
+		PUSH RDI
+		MOV RDI, @printFormatArray
+		XOR RSI, RSI
+		XOR RAX, RAX
+		PUSH DX
+		CALL printf
+		POP DX
+		POP RDI
+
+		MOV CX, 0
+
+	.printArr:
+		PUSH RDI
 		MOVSX RBX, CX
+		MOV SI, [RDI + RBX]
+		MOV RDI, @printInt
+		MOVSX RSI, SI
+		XOR RAX, RAX
+		PUSH CX
+		PUSH DX
+		CALL printf
+		POP DX
+		POP CX
+		POP RDI
+
+		ADD CX, 2
+		CMP CX, DX
+		JNE .printArr
+
+		MOV RDI, @printNewLine
+		XOR RSI, RSI
+		XOR RAX, RAX
+		CALL printf
 
 		ret
 
-@dynamicDeclCheck:
-		CMP AX, 0
-		JGE .leftNotNeg
-		CALL @declNegERROR
+@asgnArr:
+		MOV CX, 0
 
-	.leftNotNeg:
-		CMP BX, 0
-		JGE .rightNotNeg
-		CALL @declNegERROR
+	.assnLoop:
+		MOVSX RBX, CX
+		MOV AX, [RSI + RBX]
+		MOV [RDI + RBX], AX
 
-	.rightNotNeg:
-		CMP BX, AX
-		JGE .dynChecked
-		CALL @declERROR
-
-	.dynChecked:
-		MOV DX, BX
-		SUB DX, AX
-		ADD DX, 1
-		ADD DX, DX
-		MOVSX RDI, DX
+		ADD CX, 2
+		CMP CX, DX
+		JNE .assnLoop
 
 		ret
-
-@boundERROR:
-		MOV RDI, @boundPrint
-		XOR RSI, RSI
-		XOR RAX, RAX
-		CALL printf
-		MOV EDI, 0
-		CALL exit
-
-@declERROR:
-		MOV RDI, @declPrint
-		XOR RSI, RSI
-		XOR RAX, RAX
-		CALL printf
-		MOV EDI, 0
-		CALL exit
-
-@declNegERROR:
-		MOV RDI, @declNeg
-		XOR RSI, RSI
-		XOR RAX, RAX
-		CALL printf
-		MOV EDI, 0
-		CALL exit
 
 ;--------------------------------------------------------------------------------------------------
 
 section .data
-		@boundPrint : db "RUNTIME ERROR : Array out of bounds" , 10, 0
-		@declPrint : db "RUNTIME ERROR : Invalid order of bounds in dynamic array declaration" , 10, 0
-		@declNeg : db "RUNTIME ERROR : Negative bound in dynamic array declaration" , 10, 0
+		@printFormatArray : db "Output : " , 0
+		@printInt : db "%d ", 0
+		@printNewLine : db 10, 0
+		@inputIntArrPrompt : db "Enter %d array elements of integer type for range ", 0
+		@leftRange : db "%d to " , 0
+		@rightRange : db "%d" ,10, 0
+		@inputInt : db "%d", 0
