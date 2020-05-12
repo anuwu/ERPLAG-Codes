9b9d3239e9e1ea2e9987c8abf64290c88c2503d1
anwesh0304 <anwesh0304@gmail.com>
2020-05-12 16:06:10 +0530

    Added erplag executable.

--- Expr_TC/expr1.asm
+++ Expr_TC/expr1.asm
@@ -1,7 +1,6 @@
 extern printf
 extern scanf
 extern malloc
-extern free
 extern exit
 
 global main
@@ -13,9 +12,34 @@ main:
 
 		SUB RSP, 4											; making space for declaration
 
-		SUB RSP, 6											; making space for declaration
+
+		MOV RDI, @inputIntPrompt									; get_value
+		MOV RBX, -2
+		CALL @getValuePrimitive
+
+
+		MOV RDI, @inputIntPrompt									; get_value
+		MOV RBX, -4
+		CALL @getValuePrimitive
 
 		SUB RSP, 6											; making space for declaration
+
+		SUB RSP, 12											; making space for declaration
+
+
+		MOV AX, [RBP - 2]
+		MOV BX, [RBP - 4]
+		CALL @dynamicDeclCheck										; checking dynamic array declaration limits
+
+
+		PUSH BX												; saving register for malloc
+		PUSH AX												; saving register for malloc
+		CALL malloc
+		MOV [RBP - 22], RAX
+		POP AX
+		MOV [RBP - 12], AX
+		POP BX
+		MOV [RBP - 14], BX
 
 		MOV RDI, @inputIntArrPrompt
 		MOV BX, 4
@@ -40,21 +64,26 @@ main:
 		ADD DX, DX
 		CALL @printIntegerArr
 
+		MOV AX, [RBP - 12]
 		MOV CX, 4
-		MOV DX, 6
-		MOV RDI, RBP
-		SUB RDI, 16
-		MOV RSI, RBP
-		SUB RSI, 10
-		SUB DX, CX
-		ADD DX, 1
-		ADD DX, DX
-		CALL @asgnArr
+		CMP AX, CX
+		JE LABEL1
+		CALL @asgnLimERROR
 
-		MOV RDI, RBP
-		SUB RDI, 16
-		MOV CX, 4
+	LABEL1:
+		MOV BX, [RBP - 14]
 		MOV DX, 6
+		CMP BX, DX
+		JE LABEL2
+		CALL @asgnLimERROR
+
+	LABEL2:
+		MOV RDI, RBP
+		SUB RDI, 10
+		MOV[RBP - 22], RDI
+		MOV RDI, [RBP - 22]
+		MOV CX, [RBP - 12]
+		MOV DX, [RBP - 14]
 		SUB DX, CX
 		ADD DX, 1
 		ADD DX, DX
@@ -70,6 +99,58 @@ main:
 ;--------------------------------------------------------------------------------------------------
 
 ;--------------------------------------------------------------------------------------------------
+
+@dynamicDeclCheck:
+		CMP AX, 0
+		JGE .leftNotNeg
+		CALL @declNegERROR
+
+	.leftNotNeg:
+		CMP BX, 0
+		JGE .rightNotNeg
+		CALL @declNegERROR
+
+	.rightNotNeg:
+		CMP BX, AX
+		JGE .dynChecked
+		CALL @declERROR
+
+	.dynChecked:
+		MOV DX, BX
+		SUB DX, AX
+		ADD DX, 1
+		ADD DX, DX
+		MOVSX RDI, DX
+
+		ret
+
+@getValuePrimitive:
+		XOR RSI, RSI
+		XOR RAX, RAX
+		PUSH RBX
+		CALL printf
+		POP RBX
+
+		MOV RAX, RSP
+		AND RAX, 15
+		ADD RAX, 8
+		SUB RSP, RAX
+		PUSH RAX
+		MOV RDI, @inputInt										; get_value
+		MOV RSI, RSP
+		SUB RSI, 16
+		PUSH RBX
+		PUSH RSI
+		CALL scanf
+		POP RSI
+		POP RBX
+		MOV AX, [RSP - 16]
+		MOV [RBP + RBX], AX
+
+		POP RAX
+		ADD RSP, RAX
+
+		ret
 
 @printGetArrPrompt:
 		MOV SI, CX
@@ -177,26 +258,40 @@ main:
 
 		ret
 
-@asgnArr:
-		MOV CX, 0
+@declERROR:
+		MOV RDI, @declPrint
+		XOR RSI, RSI
+		XOR RAX, RAX
+		CALL printf
+		MOV EDI, 0
+		CALL exit
 
-	.assnLoop:
-		MOVSX RBX, CX
-		MOV AX, [RSI + RBX]
-		MOV [RDI + RBX], AX
+@declNegERROR:
+		MOV RDI, @declNeg
+		XOR RSI, RSI
+		XOR RAX, RAX
+		CALL printf
+		MOV EDI, 0
+		CALL exit
 
-		ADD CX, 2
-		CMP CX, DX
-		JNE .assnLoop
-
-		ret
+@asgnLimERROR:
+		MOV RDI, @asgnArgMismatch
+		XOR RSI, RSI
+		XOR RAX, RAX
+		CALL printf
+		MOV EDI, 0
+		CALL exit
 
 ;--------------------------------------------------------------------------------------------------
 
 section .data
+		@declPrint : db "ERPLAG : Runtime Error --> Invalid order of bounds in dynamic array declaration" , 10, 0
+		@declNeg : db "ERPLAG : Runtime Error --> Negative bound in dynamic array declaration" , 10, 0
+		@asgnArgMismatch: db "ERPLAG : Runtime Error --> Mismatch of limits in array assignment" , 10, 0
 		@printFormatArray : db "Output : " , 0
 		@printInt : db "%d ", 0
 		@printNewLine : db 10, 0
+		@inputIntPrompt : db "Enter an integer : " , 0
 		@inputIntArrPrompt : db "Enter %d array elements of integer type for range ", 0
 		@leftRange : db "%d to " , 0
 		@rightRange : db "%d" ,10, 0
